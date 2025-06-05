e2bc0549c09e9c46f18038fcbaa1c73ac9ac8263
KEIRMAUZ <kevinmanuelarteagaruiz@gmail.com>
2025-06-05 17:52:43 -0500

    [ADD] Se agregaron componentes

--- ejercicios-001/src/App.jsx
+++ ejercicios-001/src/App.jsx
@@ -1,3 +1,5 @@
+import Ejercicio01 from './components/ejercicio01'
+import ButtonGeneral from './components/buttonGeneral' 
 function App() {
 
   return (
@@ -6,9 +8,14 @@ function App() {
         <div className="row">
           <div className="col-12">
             <h1 className="mt-2 text-center alert alert-success">Hola UTSH</h1>
+
           </div>
         </div>
+        {/**Mandando a llamar componente eterno */}
+        <Ejercicio01/>
+        <ButtonGeneral/>
       </div>
+      
     </>
   )
 }
---
+++ ejercicios-001/src/components/ejercicio01.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Ejercicio01 () {
+  return (
+    <div className="row mt-4">
+        <div className="col-12">
+            <h2 className="h4 mt-4">
+                Ejercicio 1 - Crear componente
+            </h2>
+            <hr/>
+            <span className='d-block text-center bg-primary text-white p-4'>
+                Este es un componente
+            </span>
+            <hr />
+        </div>
+    </div>
+  )
+};
+{/**Crear un boton que se llama activar cuenta */}
+{/**Crear un componente que dentro tendra 2 componentes de forma que 2 botones
+    queden horizontal un btn azul(Crear cuenta) y el otro verde(Iniciar sesion) y en celular
+    estarn de forma horizontal TIP flex direction y flex column flex row*/}
