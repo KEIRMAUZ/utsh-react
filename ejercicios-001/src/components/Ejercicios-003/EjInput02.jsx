2aee8842e273b5502c711ffd9882d092ecf813d6
KEIRMAUZ <kevinmanuelarteagaruiz@utsh.edu.mx>
2025-06-24 20:30:21 -0500

    [ADD] Ejercicio 003

--- ejercicios-001/src/components/Ejercicios-003/EjInput02.jsx
+++ ejercicios-001/src/components/Ejercicios-003/EjInput02.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TituloEjercicio from '../TituloEjercicio'
 
 const EjInput02 = () => {
+    const [valorInput1,setValorInput1] = useState(0);
+    const [valorInput2,setValorInput2] = useState(0);
+    const [resultadoSuma,setResultadoSuma] = useState(0)
+    function cambiarValorInput1(evento){
+        setValorInput1(evento.target.value)
+    }
+    const cambiarValorInput2 = (evento)=>{
+        setValorInput2(evento.target.value)
+    }
+    function Sumar(){
+        setResultadoSuma(parseFloat(valorInput1)+parseFloat(valorInput2))
+    }
     return (
         <>
             <TituloEjercicio tituloEjercicio="Use State - Input002" />
@@ -9,15 +21,16 @@ const EjInput02 = () => {
                 <div className="col-12 mb-5">
                     <div className="input-group d-flex justify-content-around g-4">
                         <div className="form-floating ">
-                            <input type="number" id="inputNumero1" className="form-control" placeholder='Numero 1'/>
-                            <label htmlFor="inputNumero1">Numero 1</label>
+                            <input type="number" id="inputNumero1" className="form-control" placeholder='Numero 1' onChange={cambiarValorInput1} value={valorInput1}/>
+                            <label htmlFor="inputNumero1"  >Numero 1</label>
                         </div>
                         <div className="form-floating ">
-                            <input type="number" id="inputNumero2" className="form-control" placeholder='Numero 2'/>
+                            <input type="number" id="inputNumero2" className="form-control" placeholder='Numero 2' onChange={cambiarValorInput2} value={valorInput2}/>
                             <label htmlFor="inputNumero2">Numero 2</label>
                         </div>
-                        <button className="btn btn-outline-success">Sumar</button>
+                        <button className="btn btn-outline-success" onClick={Sumar}>Sumar</button>
                     </div>
+                    <p>Resultado : {resultadoSuma}</p>
                 </div>
             </div>
         </>
